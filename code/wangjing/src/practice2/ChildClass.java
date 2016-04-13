e68b018c2aaaae864b732c633cc6a2355f333733
vcex <lindalee94@outlook.com>
2016-04-13 20:36:24 +0800

    Update ChildClass.java

--- code/wangjing/src/practice2/ChildClass.java
+++ code/wangjing/src/practice2/ChildClass.java
@@ -1,20 +1,4 @@
-/*
- * ================================================================
- * Copyright 2008-2015 AMT.
- * All Rights Reserved.
- *
- * This software is the confidential and proprietary information of
- * AMT Corp. Ltd, ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with AMT.
- * 
- * 国泰君安Web App移动平台项目	
- *
- * ================================================================
- *  创建人: caoqifa
- *	创建时间: 2016年4月8日 - 上午11:36:02
- */
+ 
 package practice2;
 
 /**
